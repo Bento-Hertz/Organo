763b94cb86facb53d73fca5e1fd848147b10fa85
Bento-Hertz <bentoluiz50@gmail.com>
2023-07-11 16:59:19 -0500

    first commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+function Banner() {
+    return (
+        <header className='banner'>
+            <img src="./images/banner.png" alt="The main banner of the page"/>
+        </header>
+    )
+
+}
+
+export default Banner;
